8bce563f7a5a71d09af98663497fc5dcc24d995d
Piyush Garg <62426177+il-coder@users.noreply.github.com>
2021-09-19 18:54:19 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA-EHaLJWzXfm-DdRSOM5jk2ipULnvZyuA",
-    authDomain: "to-do-2f85c.firebaseapp.com",
-    databaseURL: "https://to-do-2f85c-default-rtdb.firebaseio.com",
-    projectId: "to-do-2f85c",
-    storageBucket: "to-do-2f85c.appspot.com",
-    messagingSenderId: "632616915576",
-    appId: "1:632616915576:web:029fe2e490f233d4c9938d",
-    measurementId: "G-FZLJSN10H3"
+    apiKey: "-",
+    authDomain: "-",
+    databaseURL: "-",
+    projectId: "-",
+    storageBucket: "-",
+    messagingSenderId: "-",
+    appId: "-",
+    measurementId: "-"
 };
 
 chrome.runtime.onMessage.addListener(
